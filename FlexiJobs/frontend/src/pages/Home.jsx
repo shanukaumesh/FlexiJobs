f2471eb8bc88b3ec85d2a8cc5db69e3b5a16f93d
shanukaumesh <shanukaumesh0@gmail.com>
2024-11-25 13:57:31 +0530

    Update footer component

--- FlexiJobs/frontend/src/pages/Home.jsx
+++ FlexiJobs/frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/Home.css'; // Updated import path for the CSS
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
-        <div className="home-container">
-            <h1>Welcome to FlexiJobs</h1>
-            <p>Find your next job here!</p>
+        <div>
+            <Footer/>
+
         </div>
     );
 };
