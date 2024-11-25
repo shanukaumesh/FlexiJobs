bde8d8f8f285f35443129d95ab4672018a8a3413
shanukaumesh <shanukaumesh0@gmail.com>
2024-11-25 15:40:50 +0530

    Backend start

---
+++ FlexiJobs/backend/connect.js
@@ -0,0 +1,36 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+// Load environment variables from the .env file
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT || 5432,
+    logging: true,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+testConnection();
+
+module.exports = sequelize;
