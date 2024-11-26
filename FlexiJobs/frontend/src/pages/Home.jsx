a5cd3d7e7ec8ad3de49d9de305f787c2817d214a
shanukaumesh <shanukaumesh0@gmail.com>
2024-11-26 15:15:04 +0530

    Update Home Hero Section

--- FlexiJobs/frontend/src/pages/Home.jsx
+++ FlexiJobs/frontend/src/pages/Home.jsx
@@ -3,13 +3,14 @@ import '../styles/Home.css'; // Updated import path for the CSS
 import Footer from '../components/Footer';
 import Header_LogOutUser from '../components/Header_LogOutUser';
 import Header_LoggedUser from '../components/Header_LoggedUser';
+import HeroSection from '../components/HeroSection';
 
 const Home = () => {
     return (
         <div>
 
             <Header_LoggedUser/>
-            <h1>Hello</h1>
+            <HeroSection/>
             <Footer/>
 
         </div>
