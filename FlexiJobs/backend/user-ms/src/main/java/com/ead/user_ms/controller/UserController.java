0589acbbfbf2e26ccd4847262cca2787de9200ad
nirmalfernando <fernandonirmal607@gmail.com>
2024-12-06 21:47:52 +0530

    Created CRUD functions of the user

--- FlexiJobs/backend/user-ms/src/main/java/com/ead/user_ms/controller/UserController.java
+++ FlexiJobs/backend/user-ms/src/main/java/com/ead/user_ms/controller/UserController.java
@@ -1,4 +1,47 @@
 package com.ead.user_ms.controller;
 
-public class UserController {
+
+import com.ead.user_ms.UserMsApplication;
+import com.ead.user_ms.data.User;
+import com.ead.user_ms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController
+{
+    @Autowired
+    private UserService obj;
+
+    @GetMapping(path = "/users")
+    public List<User> getUser()
+    {
+        return obj.getUser();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User getUserById(@PathVariable int id)
+    {
+        return obj.getUserById(id);
+    }
+
+    @PostMapping(path = "/users")
+    public User createUser(@RequestBody User user)
+    {
+        return obj.createUser(user);
+    }
+
+    @PutMapping(path = "/users")
+    public User updateUser(@RequestBody User user)
+    {
+        return obj.updateUser(user);
+    }
+
+    @DeleteMapping(path = "/users/{id}")
+    public void deleteUser(@PathVariable int id)
+    {
+        obj.deleteUser(id);
+    }
 }
--- FlexiJobs/backend/user-ms/src/main/java/com/ead/user_ms/service/UserService.java
+++ FlexiJobs/backend/user-ms/src/main/java/com/ead/user_ms/service/UserService.java
@@ -1,4 +1,55 @@
 package com.ead.user_ms.service;
 
-public class UserService {
+
+import com.ead.user_ms.data.User;
+import com.ead.user_ms.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService
+{
+    @Autowired
+    private UserRepository userRepo;
+
+
+    //get users
+    public List<User> getUser()
+    {
+        return userRepo.findAll();
+    }
+
+    //get user by their id
+    public User getUserById(int id)
+    {
+        Optional<User> course = userRepo.findById(id);
+        if(course.isPresent()){
+            return course.get();
+        }
+        return null;
+    }
+
+    //create user
+    public User createUser(User user)
+    {
+        return (User) userRepo.save(user);
+
+    }
+
+    //update user
+    public User updateUser(User user)
+    {
+        return (User) userRepo.save(user);
+    }
+
+    //delete user
+    public void deleteUser(int id)
+    {
+        userRepo.deleteById(id);
+
+
+    }
 }
