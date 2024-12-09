1c5cd81e1bf93f151bda18438976e94845f3dfa6
Thirandidesilva <Thirandidesilva2002@gmail.com>
2024-12-09 13:32:39 +0530

    Created payment operations

--- FlexiJobs/backend/payment-ms/src/main/java/com/ead/payment_ms/controller/PaymentController.java
+++ FlexiJobs/backend/payment-ms/src/main/java/com/ead/payment_ms/controller/PaymentController.java
@@ -1,4 +1,48 @@
 package com.ead.payment_ms.controller;
 
+
+import com.ead.payment_ms.data.Payment;
+import com.ead.payment_ms.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class PaymentController {
+
+    @Autowired
+    private PaymentService obj;
+
+    @GetMapping(path = "/payments")
+    public List<Payment> getPayment()
+    {
+        return obj.getPayment();
+    }
+
+    @GetMapping(path = "/payments/{id}")
+    public Payment getPaymentById(@PathVariable int id)
+    {
+        return obj.getPaymentById(id);
+    }
+
+    @PostMapping(path = "/payments")
+    public Payment createPayment(@RequestBody Payment payment)
+    {
+        return obj.createPayment(payment);
+    }
+
+    @PutMapping(path = "/payments")
+    public Payment updatePayment(@RequestBody Payment payment)
+    {
+        return obj.updatePayment(payment);
+    }
+
+    @DeleteMapping(path = "/payments/{id}")
+    public void deletePayment(@PathVariable int id)
+    {
+        obj.deletePayment(id);
+
+    }
+
 }
--- FlexiJobs/backend/payment-ms/src/main/java/com/ead/payment_ms/data/Payment.java
+++ FlexiJobs/backend/payment-ms/src/main/java/com/ead/payment_ms/data/Payment.java
@@ -1,4 +1,78 @@
 package com.ead.payment_ms.data;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "user_name", nullable = false)
+    private String username;
+
+    @Column(name = " job_title", nullable = false)
+    private String jobTitle;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "paid_date", nullable = false)
+    private Date paidDate;
+
+    @Column(name = "status")
+    private boolean status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaidDate() {
+        return paidDate;
+    }
+
+    public void setPaidDate(Date paidDate) {
+        this.paidDate = paidDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
