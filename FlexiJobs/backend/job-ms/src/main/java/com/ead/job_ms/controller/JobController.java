67d5b4a2cb8ee02e376a4ecebe96fc415bd7d382
Thirandidesilva <Thirandidesilva2002@gmail.com>
2024-12-09 13:40:21 +0530

    Created the job microservices operations

--- FlexiJobs/backend/job-ms/src/main/java/com/ead/job_ms/controller/JobController.java
+++ FlexiJobs/backend/job-ms/src/main/java/com/ead/job_ms/controller/JobController.java
@@ -1,4 +1,47 @@
 package com.ead.job_ms.controller;
 
-public class JobController {
+
+import com.ead.job_ms.data.Job;
+import com.ead.job_ms.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class JobController
+{
+    @Autowired
+    private JobService obj;
+
+    @GetMapping(path = "/jobs")
+    public List<Job> getJob()
+    {
+        return obj.getJob();
+    }
+
+    @GetMapping(path = "/jobs/{id}")
+    public Job getJobById(@PathVariable int id)
+    {
+        return obj.getJobById(id);
+    }
+
+    @PostMapping(path = "/jobs")
+    public Job createJob(@RequestBody Job job)
+    {
+        return obj.createJob(job);
+    }
+
+    @PutMapping(path = "/jobs")
+    public Job updateJob(@RequestBody Job job)
+    {
+        return obj.updateJob(job);
+    }
+
+    @DeleteMapping(path = "/jobs/{id}")
+    public void deleteJob(@PathVariable int id)
+    {
+        obj.deleteJob(id);
+
+    }
 }
--- FlexiJobs/backend/job-ms/src/main/java/com/ead/job_ms/data/Job.java
+++ FlexiJobs/backend/job-ms/src/main/java/com/ead/job_ms/data/Job.java
@@ -1,4 +1,166 @@
 package com.ead.job_ms.data;
 
+
+import jakarta.persistence.*;
+
+import javax.management.relation.Role;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
 public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "job_type", nullable = false)
+    private String jobTitle;
+
+    @Column(name = "job_role")
+    private String jobRole;
+
+    @Column(name = "district")
+    private  String district;
+
+    @Column(name = "city")
+    private  String city;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "application_receiving")
+    private ApplicationReceiving applicationReceiving;
+
+    @Column(name = "education")
+    private String education;
+
+    @Column(name = "starting_salary")
+    private BigDecimal startingSalary;
+
+    @Column(name = "salary_limit")
+    private BigDecimal salaryLimit;
+
+    @Column(name = "application_deadline")
+    private Date applicationDeadline;
+
+    @Column(name = " job_status")
+    private boolean jobStatus;
+
+    @Column(name = "job_photo")
+    private String jobPhoto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobRole() {
+        return jobRole;
+    }
+
+    public void setJobRole(String jobRole) {
+        this.jobRole = jobRole;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public BigDecimal getStartingSalary() {
+        return startingSalary;
+    }
+
+    public void setStartingSalary(BigDecimal startingSalary) {
+        this.startingSalary = startingSalary;
+    }
+
+    public BigDecimal getSalaryLimit() {
+        return salaryLimit;
+    }
+
+    public void setSalaryLimit(BigDecimal salaryLimit) {
+        this.salaryLimit = salaryLimit;
+    }
+
+    public Date getApplicationDeadline() {
+        return applicationDeadline;
+    }
+
+    public void setApplicationDeadline(Date applicationDeadline) {
+        this.applicationDeadline = applicationDeadline;
+    }
+
+    public boolean isJobStatus() {
+        return jobStatus;
+    }
+
+    public void setJobStatus(boolean jobStatus) {
+        this.jobStatus = jobStatus;
+    }
+
+    public String getJobPhoto() {
+        return jobPhoto;
+    }
+
+    public void setJobPhoto(String jobPhoto) {
+        this.jobPhoto = jobPhoto;
+    }
+
+    public enum ApplicationReceiving
+    {
+        email,dashboard
+    }
 }
